2179a20a01091fde9177b5f4399868fa2ed4e479
Santiago3102 <santiago.gomez3@utp.edu.co>
2024-07-08 01:28:31 -0500

    Implementación de markdown en la descripcion de las tareas e instalamos la dependencia marked necesaria para esto.

--- src/routes/task-routes.js
+++ src/routes/task-routes.js
@@ -2,12 +2,12 @@ import { Router } from "express";
 import bcrypt from 'bcrypt';
 import jwt from 'jwt-simple';
 import { pool } from "../db.js"
+import { marked } from 'marked';
 
 const secret = 'aguapanelaconlimon10';
 const taskRouter = Router();
 
 taskRouter.get('/task', async (req, res) => {
-
     try {
         const result = await pool.query('SELECT * FROM tasks');
         res.status(200).json(result.rows);
@@ -19,34 +19,41 @@ taskRouter.get('/task', async (req, res) => {
 
 taskRouter.post('/task', async (req, res) => {
     const { title, body, id_section } = req.body;
+    
+    // Convertir Markdown a HTML
+    const htmlBody = marked(body);
   
     try {
         const result = await pool.query(
             'INSERT INTO tasks (title, body, id_section) VALUES ($1, $2, $3) RETURNING *',
-            [title, body, id_section]
+            [title, htmlBody, id_section]
         );
         res.status(201).json(result.rows[0]);
     } catch (err) {
         console.error(err);
         res.status(500).send('Error creating task');
     }
-  });
+});
 
-  taskRouter.put('/task', async (req, res)=>{
+taskRouter.put('/task', async (req, res) => {
     const {title, body, id_section} = req.body
-    try{
+    
+    // Convertir Markdown a HTML
+    const htmlBody = marked(body);
+
+    try {
         const result = await pool.query(
             'UPDATE tasks set title=$1, body=$2, id_section=$3', 
-            [title, body, id_section]
+            [title, htmlBody, id_section]
         );
         res.status(200).json(result.rows[0]);
-    }catch (err){
+    } catch (err) {
         console.error(err);
         res.status(500).send('Error updating task')
     }
-  });
+});
 
-  taskRouter.delete('/task/:id', async (req, res)=>{
+taskRouter.delete('/task/:id', async (req, res) => {
     const {id} = req.params
     try {
         const result = await pool.query(
@@ -57,6 +64,6 @@ taskRouter.post('/task', async (req, res) => {
         console.error(err);
         res.status(500).send('Error deleting task')
     }
-  })
+});
 
 export default taskRouter;
