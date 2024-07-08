1d46777fd4f99c3b399a58eca06f008629f95c3d
JuanF-Cano <173503745+JuanF-Cano@users.noreply.github.com>
2024-07-07 23:03:10 -0500

    Update user-routes.js

--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,26 +1,10 @@
 import { Router } from "express";
 import bcrypt from 'bcrypt';
-import jwt from 'jwt-simple';
+import { SignJWT, jwtVerify } from 'jose';
 import { pool } from "../db.js"
 
 const secret = 'aguapanelaconlimon10';
 const userRouter = Router();
-
-/* const checkToken = (req, res, next) => {
-    const token = req.headers['authorization'];
-    const { authorization } = req.headers[];
-
-    if (token) {
-        try {
-            const state = jwt.compare(token, )
-            next();
-        } catch (err) {
-            res.status(401).send('Invalid token');
-        }
-    } else {
-        res.status(401).send('Token required');
-    }
-}; 
 
 userRouter.get('/users', async (req, res) => {
     try {
@@ -48,39 +32,99 @@ userRouter.post('/users', async (req, res) => {
     }
   });
 
-  userRouter.post('/login', checkToken, async (req, res) => {
-    const { name, password } = req.body;
-
+  userRouter.post("/login", async (req, res) => {
+    const { email, password } = req.body;
+  
+    if (!email || !password) return res.status(400).send('Email y contraseña son requeridos');
+  
     try {
-        const result = await pool.query('SELECT * FROM users WHERE name = $1', [name]);
-
-        if (result.rows.length > 0) {
-            const user = result.rows[0];
-            const match = await bcrypt.compare(password, user.password);
-
-            if (match) {
-                const payload = { id: user.id, username: user.username };
-                const token = jwt.encode(payload, secret);
-                res.json({ token });
-            } else {
-                res.status(401).send('Invalid username or password');
-            }
-        } else {
-            res.status(401).send('Invalid username or password');
-        }
+        const result = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+        const user = result.rows[0];
+        
+        if (!user) return res.status(401).send('Email inválido');
+        
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) return res.status(401).send('Contraseña inválida');
+  
+        const encoder = new TextEncoder();
+        const id_users = `${user.id_users}`;
+        const jwtConstructor = new SignJWT({ id_users });
+        const jwt = await jwtConstructor
+            .setProtectedHeader({ alg: 'HS256', typ: 'JWT' })
+            .setIssuedAt()
+            .setExpirationTime('1h')
+            .sign(encoder.encode(secret));
+        
+        return res.send({ jwt });
     } catch (err) {
         console.error(err);
-        res.status(500).send('Error logging in');
+        res.status(500).send('Error al iniciar sesión');
     }
 });
 
+userRouter.get("/login", async (req, res) => {
+    const { authorization } = req.headers;
+    console.log(authorization)
+    if (!authorization) {
+        return res.status(401).send('Token no proporcionado');
+    }
 
-/* userRouter.delete('/users/:id', (req, res) => {
-    res.send("funciona");
+    try {
+        const encoder = new TextEncoder();
+        const { payload } = await jwtVerify(authorization, encoder.encode(secret));
+        console.log(payload)
+        const result = await pool.query('SELECT * FROM users WHERE id_users = $1', [payload.id_users]);
+        const user = result.rows[0];
+        
+        if (!user) return res.status(401).send('Usuario no encontrado');
+        
+        delete user.password;
+
+        return res.send(user);
+    } catch (err) {
+        console.error(err);
+        return res.status(401).send('Token inválido o expirado');
+    }
 });
 
-userRouter.put('/users/:id', (req, res) => {
-    res.send("funciona");
-}); */
+userRouter.delete('/users/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query('DELETE FROM users WHERE id_users = $1 RETURNING *', [id]);
+        const user = result.rows[0];
+
+        if (!user) return res.status(404).send('Usuario no encontrado');
+        
+        return res.status(200).json(user);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error al eliminar el usuario');
+    }
+});
+
+userRouter.put('/users/:id', async (req, res) => {
+    const { id } = req.params;
+    const { name, email, password } = req.body;
+
+    if(!name || !email || !password) return res.status(401).send('Faltan datos');
+
+    try {
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const result = await pool.query(
+            'UPDATE users SET name = $1, email = $2, password = $3 WHERE id_users = $4 RETURNING *',
+            [name, email, hashedPassword, id]
+        );
+        const user = result.rows[0];
+
+        if (!user) return res.status(404).send('Usuario no encontrado');
+
+        delete user.password;
+
+        return res.status(200).json(user);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error al actualizar el usuario');
+    }
+});
 
 export default userRouter;
